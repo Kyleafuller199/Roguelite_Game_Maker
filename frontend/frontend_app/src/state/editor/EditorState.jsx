c65870b77b2598efc3a73959093f1e2d6dc2bfce
xXAash <ashkarx@oregonstate.edu>
2026-01-29 13:38:42 -0800

    Use @ as a path alias

--- frontend/frontend_app/src/state/editor/EditorState.jsx
+++ frontend/frontend_app/src/state/editor/EditorState.jsx
@@ -1,12 +1,12 @@
 // src/state/editor/EditorState.jsx
 import React, { createContext, useContext, useMemo, useState, useEffect } from "react";
 
-import { initialState } from "./InitialState";
+import { initialState } from "@/InitialState";
 
-import { createCard, updateSelectedCard } from "./assets/cards";
-import { createRelic, updateSelectedRelic } from "./assets/relics";
-import { createPotion, updateSelectedPotion } from "./assets/potions";
-import { createEnemy, updateSelectedEnemy } from "./assets/enemies";
+import { createCard, updateSelectedCard } from "@/assets/cards";
+import { createRelic, updateSelectedRelic } from "@/assets/relics";
+import { createPotion, updateSelectedPotion } from "@/assets/potions";
+import { createEnemy, updateSelectedEnemy } from "@/assets/enemies";
 
 
 const EditorContext = createContext(null);
